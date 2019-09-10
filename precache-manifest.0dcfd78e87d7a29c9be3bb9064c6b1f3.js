a7762734e9949e2a60f0da7980ba21fce0e7c8fc
ALEXIS RAJIS <arajis06@gmail.com>
2019-09-09 22:04:41 -0400

    Updates

---
+++ precache-manifest.0dcfd78e87d7a29c9be3bb9064c6b1f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d4d05ac893a7359517c233775c49ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22e8a22c1330485fc60d",
+    "url": "/static/css/main.b60df18f.chunk.css"
+  },
+  {
+    "revision": "77efd56f78871ce6da77",
+    "url": "/static/js/2.7fcebd89.chunk.js"
+  },
+  {
+    "revision": "22e8a22c1330485fc60d",
+    "url": "/static/js/main.c727ed6a.chunk.js"
+  },
+  {
+    "revision": "3970621b7491c85dcccf",
+    "url": "/static/js/runtime~main.5bd82b68.js"
+  }
+]);
